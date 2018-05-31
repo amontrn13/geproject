00b4701460ae9284141da63b18d1b73dda887e46
Mario Chinchilla <cs.mchinchilla@gmail.com>
2018-05-31 11:35:44 -0600

    Arreglo Final

---
+++ GE/Orden.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GE
+{
+    
+    class Orden
+    {
+        public int ID { get; set; }
+        public String Cliente { get; set; }
+        public String Codigo { get; set; }
+
+        public Orden(int ID, String Cliente, String Codigo) {
+            this.ID = ID;
+            this.Cliente = Cliente;
+            this.Codigo = Codigo;
+        }
+    }
+}
