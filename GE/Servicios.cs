81e41bb01c689418a76d4da2f9add294123a78f2
sami94 <samyprlt@gmail.com>
2018-06-02 12:57:30 -0600

    Agregar servicio

--- GE/Servicios.cs
+++ GE/Servicios.cs
@@ -8,8 +8,19 @@ namespace GE
 {
     class Servicios
     {
-       
+     
+        public String Servicio { get; set; }
+        public String Id_Servicio { get; set; }
+        public int Horas { get; set; }
+        public int Comision { get; set; }
 
+        public Servicios(String Servicio, String Id_Servicio, int Horas, int Comision)
+        {
+            this.Servicio = Servicio;
+            this.Id_Servicio = Id_Servicio;
+            this.Horas = Horas;
+            this.Comision = Comision;
+        }
 
     }
 }
